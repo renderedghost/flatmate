2832c31a856b3f3c3defa81add991c645e351109
Barry Prendergast <barry.m.prendergast@gmail.com>
2024-06-13 23:14:59 +0200

    refactor: Update package dependencies
    - Update dependencies for @storybook/cli
    - Update dependencies for @storybook/test
    - Update dependencies for @storybook/addon-interactions

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Thread from '../components/thread'; // Adjust the import path as necessary
 
 const HomePage: React.FC = () => {
-  return <div>Welcome to FlatMate</div>;
+  return (
+    <div>
+      <h1>Welcome to FlatMate</h1>
+      <Thread />
+    </div>
+  );
 };
 
 export default HomePage;
